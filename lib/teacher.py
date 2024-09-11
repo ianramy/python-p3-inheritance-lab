fe80e73db42f6068b22f89c6dd98149c363268ba
ianramy <imwagore@gmail.com>
2024-09-12 01:27:39 +0300

    modified

--- lib/teacher.py
+++ lib/teacher.py
@@ -1,10 +1,12 @@
-#!/usr/bin/env python
-
 from user import User
 
 import random
 
 class Teacher(User):
+    def __init__(self, first_name, last_name):
+        super().__init__(first_name, last_name)
+        self.knowledge = ["Teaching Methodology"]
 
     def teach(self):
-        pass
+        # Return the first item from knowledge for testing purposes
+        return self.knowledge[0]
